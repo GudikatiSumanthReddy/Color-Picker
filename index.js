e83932ec43a9b3c7de2b8f5b9e9eebf35d339f58
gudikati sumanth reddy <sumanthreddygudikati@gmail.com>
2024-03-05 20:29:12 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let backgroundcolor = document.getElementById("totalbackgroundcolor");
+let text = document.getElementById("text-editor");
+
+
+
+function tochangebackgroundcolorgray(){
+    backgroundcolor.style.backgroundColor="#e0e0e0";
+    text.textContent = "#e0e0e0";
+}
+
+
+function tochangebackgroundcolorgreen(){
+    backgroundcolor.style.backgroundColor="#6fcf97";
+    text.textContent = "#6fcf97";
+}
+
+function tochangebackgroundcolorblue(){
+    backgroundcolor.style.backgroundColor="#56ccf2";
+    text.textContent = "#56ccf2";
+}
+
+function tochangebackgroundcolorpink(){
+    backgroundcolor.style.backgroundColor="#bb6bd9";
+    text.textContent = "#bb6bd9";
+}
